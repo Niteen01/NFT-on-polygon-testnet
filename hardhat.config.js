8c66d0b909afd76d54bc5b6edfbc51c28403ba3e
Niteen01 <94301070+Niteen01@users.noreply.github.com>
2021-12-15 23:32:43 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require('fs');
+// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    /*
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://gugmjh8rpmvu.usemoralis.com:2053/server",
+      accounts: [privateKey]
+    },
+    */
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
